6ce7fa18ecd66f8a4bea4fbca6ffb86465fa8e9c
Brian Hill <brianhill.dev@gmail.com>
2022-08-22 17:57:00 -0600

    added table

--- product_dashboard/catalog/views.py
+++ product_dashboard/catalog/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
 from .models import Category, Tag, Product
+from .filters import ProductFilter
+from django.views.generic import ListView, DetailView
 
 
 def index(request):
     num_products = Product.objects.all().count()
     num_tags = Tag.objects.all().count()
     num_categories = Category.objects.all().count()
+    products = Product.objects.all()
+    # myfilter = ProductFilter(request.GET, queryset=Product)
+    # products = myfilter.qs
 
     context = {
         'num_products': num_products,
         'num_categories': num_categories,
-        'num_tags': num_tags
+        'num_tags': num_tags,
+        'products': products,
+        # 'myfilter': myfilter
     }
 
     return render(request, 'index.html', context=context)
+
+
